61f489ad74b610407d75a9815fbd060e42b42d72
ArifahZhafirah <arifahzhafirah206@gmail.com>
2024-09-27 11:05:58 +0700

    Add files via upload

---
+++ PemilihanBilangan24.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PemilihanBilangan24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = sc.nextInt();
+        
+        String hasil = (angka % 2 == 0) ? "angka " + angka + " termasuk genap" : "angka " + angka + " termasuk ganjil";
+
+        System.out.println(hasil);
+    }
+}
